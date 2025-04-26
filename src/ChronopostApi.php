6bb88174c3adc20fdddb50aceed4054d6bb8352b
Jérémy Munsch <github@jeremydev.ovh>
2025-04-26 16:47:19 +0200

    refactor: ChronopostApi add quickcost call

--- src/ChronopostApi.php
+++ src/ChronopostApi.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Kwaadpepper\ChronopostApiPhp;
 
+use Kwaadpepper\ChronopostApiPhp\Dto\QuickCostV3;
+use Kwaadpepper\ChronopostApiPhp\Enums\ShippingType;
+use Kwaadpepper\ChronopostApiPhp\ObjectValues\AccountNumber;
+use Kwaadpepper\ChronopostApiPhp\ObjectValues\Password;
+use Kwaadpepper\ChronopostApiPhp\ObjectValues\PostCode;
+use Kwaadpepper\ChronopostApiPhp\ObjectValues\ProductCode;
 use Kwaadpepper\ChronopostApiPhp\ObjectValues\TrackingNumber;
+use Kwaadpepper\ChronopostApiPhp\Services\Cost\QuickCostService;
 use Kwaadpepper\ChronopostApiPhp\Services\Tracking\TrackSearchService;
 use WsdlToPhp\PackageBase\SoapClientInterface;
 
@@ -12,22 +19,26 @@ class ChronopostApi
 {
     private TrackSearchService $trackSearchService;
 
+    private QuickCostService $quickCostService;
+
+
     /**
-     * Summary of __construct
+     * Constructor
      *
-     * @param string $accountNumber A chronopost account number.
-     * @param string $password      A chronopost api password for the accountm received by a commercial from chronopost.
+     * @param \Kwaadpepper\ChronopostApiPhp\ObjectValues\AccountNumber $accountNumber The account number.
+     * @param \Kwaadpepper\ChronopostApiPhp\ObjectValues\Password      $password      The password.
      */
     public function __construct(
-        #[\SensitiveParameter] string $accountNumber,
-        #[\SensitiveParameter] string $password
+        #[\SensitiveParameter] private AccountNumber $accountNumber,
+        #[\SensitiveParameter] private Password $password
     ) {
         $defaultSopapOptions = [
-            SoapClientInterface::WSDL_LOGIN => $accountNumber,
-            SoapClientInterface::WSDL_PASSWORD => $password,
+            SoapClientInterface::WSDL_LOGIN => $accountNumber->getAccountNumber(),
+            SoapClientInterface::WSDL_PASSWORD => $password->getPassword(),
         ];
 
         $this->trackSearchService = new TrackSearchService($defaultSopapOptions);
+        $this->quickCostService   = new QuickCostService($defaultSopapOptions);
     }
 
     /**
@@ -44,5 +55,37 @@ class ChronopostApi
     public function trackSingleShipment(TrackingNumber $trackingNumber): array
     {
         return $this->trackSearchService->findUsingTrackingNumber($trackingNumber);
+    }
+
+    /**
+     * Estimate the shipping cost for a shipment.
+     *
+     * @param \Kwaadpepper\ChronopostApiPhp\ObjectValues\PostCode    $from         The sender's postal code.
+     * @param \Kwaadpepper\ChronopostApiPhp\ObjectValues\PostCode    $to           The recipient's postal code.
+     * @param integer                                                $weight       The weight of the shipment in grams.
+     * @param \Kwaadpepper\ChronopostApiPhp\ObjectValues\ProductCode $productCode  The product code for the shipment.
+     * @param \Kwaadpepper\ChronopostApiPhp\Enums\ShippingType       $shippingType The shipping type.
+     *
+     * @return \Kwaadpepper\ChronopostApiPhp\Dto\QuickCostV3 The estimated shipping cost.
+     *
+     * @throws \Kwaadpepper\ChronopostApiPhp\Exceptions\ApiError                     If the API call fails.
+     * @throws \Kwaadpepper\ChronopostApiPhp\Exceptions\QuickCost\QuickCostException If the API returns an error.
+     */
+    public function estimateShippingCost(
+        PostCode $from,
+        PostCode $to,
+        int $weight,
+        ProductCode $productCode,
+        ShippingType $shippingType
+    ): QuickCostV3 {
+        return $this->quickCostService->quickCostV3(
+            $this->accountNumber,
+            $this->password,
+            $from,
+            $to,
+            $weight,
+            $productCode,
+            $shippingType
+        );
     }
 }
