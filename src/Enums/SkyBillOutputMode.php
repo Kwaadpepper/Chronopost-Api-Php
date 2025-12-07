80ed82cbf5fb72f5416d962e28b5c4d7cfa7fb0c
Jérémy Munsch <github@jeremydev.ovh>
2025-12-07 19:00:40 +0100

    feat: Update SkyBillOutputMode enum cases
    Refactor enum cases for clarity and consistency in naming.

--- src/Enums/SkyBillOutputMode.php
+++ src/Enums/SkyBillOutputMode.php
@@ -6,11 +6,30 @@ namespace Kwaadpepper\ChronopostApiPhp\Enums;
 
 enum SkyBillOutputMode: int
 {
-    case TO_SHIPPER_MAIL = 0;
+    /**
+     *  la LT est envoyée à l'adresse mail
+     *  shipperEmail. Si la structure
+     *  esdValue est renseignée, ce mail
+     *  servira à confirmer l'enlèvement et à
+     *  envoyer les LT
+     */
+    case SHIPPER_MAIL_SENDING = 1;
 
-    case NO_MAIL_SENDING = 1;
+    /**
+     * pas d'envoi de mail
+     */
+    case NO_MAIL_SENDING = 2;
 
-    case POST_OFFICE_AND_SMS_PRINTABLE = 2;
+    /**
+     * la LT peut être imprimée en
+     * Bureau de Poste, et envoi de SMS
+     */
+    case POST_OFFICE_AND_SMS_PRINTABLE = 3;
 
-    case SHOP2SHOP_EMAIL = 3;
+    /**
+     * la LT est envoyée à l'adresse
+     * mail shipperEmail et au
+     * destinataire final (shop2shop)
+     */
+    case SHOP2SHOP_EMAIL = 4;
 }
